fde9ab8ded51ed69f299a6d2f65ffaba61e46d4b
Louis Wong <154124680+Louis115@users.noreply.github.com>
2024-11-03 15:30:29 +0800

    Update SwiftPencilKitPlugin.swift

--- ios/Classes/SwiftPencilKitPlugin.swift
+++ ios/Classes/SwiftPencilKitPlugin.swift
@@ -2,28 +2,41 @@ import Flutter
 import UIKit
 
 public class SwiftPencilKitPlugin: NSObject, FlutterPlugin {
+  private static var channel: FlutterMethodChannel?
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     registrar.register(
       FLPencilKitFactory(messenger: registrar.messenger()),
       withId: "plugins.mjstudio/flutter_pencil_kit"
     )
 
-    let channel = FlutterMethodChannel(
+    channel = FlutterMethodChannel(
       name: "plugins.mjstudio/flutter_pencil_kit/util",
       binaryMessenger: registrar.messenger()
     )
 
-    channel.setMethodCallHandler {
-      (call: FlutterMethodCall, result: @escaping FlutterResult) in
+    channel?.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
       PencilKitUtil.handleMethodCall(call: call, result: result)
     }
+  }
+
+  public static func deregister() {
+    // Remove method call handler to ensure complete cleanup
+    channel?.setMethodCallHandler(nil)
+    channel = nil
   }
 }
 
 private enum PencilKitUtil {
   static func handleMethodCall(call: FlutterMethodCall, result: @escaping FlutterResult) {
     if call.method == "checkAvailable" {
-      result(ProcessInfo().operatingSystemVersion.majorVersion >= 13)
+      if #available(iOS 13.0, *) {
+        result(true)
+      } else {
+        result(false)
+      }
+    } else {
+      result(FlutterMethodNotImplemented)
     }
   }
 }
